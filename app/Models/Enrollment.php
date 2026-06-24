7bf19489731dbf7c98ee7a3366940663fdb55691
Kevin Bartolome <liezelbart142029@gmail.com>
2026-06-24 13:51:37 +0800

    Added fillable for enrollment so it protects the data that comes

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
 {
+    protected $fillable = ['student_id', 'subject_id'];
+    
     public function student()
     {
         return $this->belongsTo(User::class, 'student_id');
