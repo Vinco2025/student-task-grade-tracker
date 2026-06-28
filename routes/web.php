a325aacc6aa9f8056311331083a070c37c13f24d
Kevin Bartolome <liezelbart142029@gmail.com>
2026-06-29 05:00:59 +0800

    Added control for authentication for subjects

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\GradeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SubmissionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,14 +27,23 @@ Route::get('/admin/test', function () {
     return 'Welcome, Admin! This pade is protected.';
 })->middleware(['auth', 'role:admin']);
 
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::resource('subjects', SubjectController::class)->except(['index', 'show']);
+});
+
 Route::middleware(['auth'])->group(function () {
-    Route::resource('subjects', SubjectController::class);
+    Route::resource('subjects', SubjectController::class)->only(['index', 'show']);
     Route::resource('subjects.tasks', TaskController::class)->shallow();
 });
 
 Route::middleware(['auth', 'role:teacher'])->group(function () {
     Route::get('/tasks/{task}/grades', [GradeController::class, 'edit'])->name('grades.edit');
     Route::post('/tasks/{task}/grades', [GradeController::class, 'update'])->name('grades.update');
+});
+
+Route::middleware(['auth', 'role:student'])->group(function () {
+    Route::get('/tasks/{task}/submission', [SubmissionController::class, 'edit'])->name('tasks.submissions.edit');
+    Route::post('/tasks/{task}/submission', [SubmissionController::class, 'update'])->name('tasks.submissions.update');
 });
 
 require __DIR__.'/auth.php';
